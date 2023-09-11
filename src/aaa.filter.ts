0efa3fde539e76f265275db07fbc003a84a20ab1
dongyuekai <457545648@qq.com>
2023-09-11 14:59:08 +0800

    feat：Exception Filter跨多种上下文复用

--- src/aaa.filter.ts
+++ src/aaa.filter.ts
@@ -1,9 +1,23 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { Response } from 'express';
 import { AaaException } from './AaaException';
 
 @Catch(AaaException)
-export class AaaFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {
-    console.log(exception, host);
+export class AaaFilter implements ExceptionFilter {
+  // ArgumentHost是用于切换http ws rpc等上下文类型的
+  catch(exception: AaaException, host: ArgumentsHost) {
+    // host;
+    // console.log('dyk===exception===', exception, 'dyk===host====', host);
+    if (host.getType() === 'http') {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse<Response>();
+      const request = ctx.getRequest<Request>();
+      response.status(500).json({
+        aaa: exception.aaa,
+        bbb: exception.bbb,
+      });
+    } else if (host.getType() === 'ws') {
+    } else if (host.getType() === 'rpc') {
+    }
   }
 }
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,8 +8,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @UseFilters(AaaFilter)
+  @UseFilters(AaaFilter) // 路由级别启用AaaFilter 并且在handler里抛了一个AaaException类型的异常 也可以在main.ts中全局启用
   getHello(): string {
+    // 启用异常
     throw new AaaException('aaa', 'bbb');
     return this.appService.getHello();
   }
